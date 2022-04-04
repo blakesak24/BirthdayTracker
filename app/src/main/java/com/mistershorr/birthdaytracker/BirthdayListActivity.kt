07c49abd59333d7313cdd81c2cb0523f5078b2d7
blakesak24 <blakesakamoto8@gmail.com>
2022-04-04 10:08:42 -0700

    4/4

--- app/src/main/java/com/mistershorr/birthdaytracker/BirthdayListActivity.kt
+++ app/src/main/java/com/mistershorr/birthdaytracker/BirthdayListActivity.kt
@@ -3,19 +3,24 @@ package com.mistershorr.birthdaytracker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mistershorr.birthdaytracker.databinding.ActivityBirthdayListBinding
 import com.backendless.exceptions.BackendlessFault
 
 import com.backendless.async.callback.AsyncCallback
 
 import com.backendless.Backendless
+import com.backendless.persistence.DataQueryBuilder
+
+
+
 
 
 
 
 class BirthdayListActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityBirthdayListBinding
+    private lateinit var binding : ActivityBirthdayListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +31,19 @@ class BirthdayListActivity : AppCompatActivity() {
     }
 
     private fun loadDataFromBackendless() {
+            val objectId = Backendless.UserService.CurrentUser().objectId
+        val whereClause = "ownerId = '$objectId'"
+        val queryBuilder = DataQueryBuilder.create()
+        queryBuilder.whereClause = whereClause
+
 
         Backendless.Data.of(Person::class.java).find(object : AsyncCallback<List<Person?>?> {
             override fun handleResponse(foundPeople: List<Person?>?) {
                 // every loaded object from the "Contact" table is now an individual java.util.Map
                 Log.d("BirthdayList", "handleResponse: ${foundPeople}")
+                val adapter = BirthdayAdapter(((foundPeople ?: listOf()) as List<Person>))
+                binding.recyclerViewBirthdayListPeople.adapter = adapter
+                binding.recyclerViewBirthdayListPeople.layoutManager= LinearLayoutManager(this@BirthdayListActivity)
             }
 
             override fun handleFault(fault: BackendlessFault) {
